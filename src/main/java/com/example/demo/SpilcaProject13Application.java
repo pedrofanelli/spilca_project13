2acb929f955319ffc3eb7dabe24c31b14320590c
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-01 19:02:01 -0300

    working

--- src/main/java/com/example/demo/SpilcaProject13Application.java
+++ src/main/java/com/example/demo/SpilcaProject13Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages= {"controllers"})
+@ComponentScan(basePackages= {"controllers","exceptionHandlers","services"})
 public class SpilcaProject13Application {
 
 	public static void main(String[] args) {
