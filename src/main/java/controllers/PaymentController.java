25088b690002a596cc6868ff4036022b5ed0da96
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-01 18:51:34 -0300

    controller

---
+++ src/main/java/controllers/PaymentController.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import models.PaymentDetails;
+import services.PaymentService;
+
+@RestController
+public class PaymentController {
+
+	private final PaymentService paymentService;
+	
+	// DI
+	public PaymentController(
+			PaymentService paymentService) {
+		this.paymentService = paymentService;
+	}
+	
+	/*
+	 * We can handle the exceptions with a try and catch directly in the mapping, or 
+	 * using some ASPECTS, let's do that.
+	 * Here we only focus on the positive responses, then the negative on the aspects
+	 */
+	
+	@PostMapping("/payment")
+	public ResponseEntity<PaymentDetails> makePayment() {
+		PaymentDetails paymentDetails = paymentService.processPayment();
+		return ResponseEntity
+				.status(HttpStatus.ACCEPTED)
+				.body(paymentDetails);
+	}
+	
+}
