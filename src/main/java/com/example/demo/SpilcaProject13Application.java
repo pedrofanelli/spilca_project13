d9fb218191dc1333349f0b7ce8b5374b349039ae
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-01 18:32:47 -0300

    init

---
+++ src/main/java/com/example/demo/SpilcaProject13Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpilcaProject13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpilcaProject13Application.class, args);
+	}
+
+}
